a3f60a4d437bd7fce74f9a974b99cfe393124f6f
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-11-06 02:29:10 -0300

    Implementação da camada de domínio

---
+++ src/main/java/edu/gui/domain/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package edu.gui.domain.repository;
+
+
+import edu.gui.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
